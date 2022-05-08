1c9bbaadd69ab65cbac8796284fdef286fc91bb0
Jonathan Sifontes <jnthan.2412@gmail.com>
2022-05-08 16:21:32 -0600

    funciona! :D

--- src/php/data_codigo_clase.php
+++ src/php/data_codigo_clase.php
@@ -1,139 +1,68 @@
 <?php
-echo "hola";
 interface Datos{
   public function Convertir();
-
 }
 
 class Padre implements Datos {
-public $bytes = 1;
-public $kilobytes = 1024;
-public $megabytes = 1048576;
-public $gigabytes = 1073741824;
-public $terabytes = 1099511627776;
-public $enviar;
-public $cantidad;
-public $from;
-public $to;
-
-public function Convertir(){
-  if (isset($_POST['enviar'])){
-    $this->cantidad = $_POST['cantidad'];   
-    $this->from = $_POST['from_unit'];
-    $this->to = $_POST['to_unit'];
-//BYTES A OTRAS UNIDADES    
-    if ($this->from=="Bytes" & $this->to=="Bytes") {
-        echo $this->cantidad . " ". $this->from . " = " . $this->cantidad . " " . $this->to;
-        }
-    elseif ($this->from=="Bytes" & $this->to=="Kilobytes") {
-        $resultado = $this->cantidad / $this->kilobytes;
-        echo $this->cantidad . " ". $this->from . " = " . rtrim(number_format($resultado,8,".",",")) . " ". $this->to;
-        }
-    elseif ($this->from=="Bytes" & $this->to=="Megabytes") {
-      $resultado = $this->cantidad/$this->megabytes;
-      echo $this->cantidad . " ". $this->from . " = " . rtrim(number_format($resultado,16,".",",")) . " ". $this->to;
-        }
-    elseif ($this->from=="Bytes" & $this->to=="Gigabytes") {
-      $resultado = $this->cantidad/$this->gigabytes;
-      echo $this->cantidad . " ". $this->from . " = " . rtrim(number_format($resultado,16,".",",")) . " ". $this->to;
-        }
-    elseif ($this->from=="Bytes" & $this->to=="Terabytes") {
-      $resultado = $this->cantidad/$this->terabytes;
-      echo $this->cantidad . " ". $this->from . " = " . rtrim(number_format($resultado,16,".",",")) . " ". $this->to;
-        }
-//KILOBYTES A OTRAS UNIDADES        
-      elseif ($this->from=="Kilobytes" & $this->to=="Bytes") {
-        $resultado = $this->cantidad * $this->kilobytes;
-        echo $this->cantidad . " ". $this->from . " = " . rtrim(number_format($resultado,0,".",",")) . " ". $this->to;
-        }
-      elseif ($this->from=="Kilobytes" & $this->to=="Kilobytes") {
-        echo $this->cantidad . " ". $this->from . " = " . $this->cantidad . " " . $this->to;
-        }
-      elseif ($this->from=="Kilobytes" & $this->to=="Megabytes") {
-        $resultado = $this->cantidad / $this->kilobytes;
-        echo $this->cantidad . " ". $this->from . " = " . rtrim(number_format($resultado,6,".",",")) . " ". $this->to;
-        }
-      elseif ($this->from=="Kilobytes" & $this->to=="Gigabytes") {
-        $resultado = $this->cantidad / $this->megabytes;
-        echo $this->cantidad . " ". $this->from . " = " . rtrim(number_format($resultado,10,".",",")) . " ". $this->to;
-        }
-      elseif ($this->from=="Kilobytes" & $this->to=="Terabytes") {
-        $resultado = $this->cantidad / $this->gigabytes;
-        echo $this->cantidad . " ". $this->from . " = " . rtrim(number_format($resultado,12,".",",")) . " ". $this->to;
-        }
-//MEGABYTES A OTRAS UNIDADES        
-      elseif ($this->from=="Megabytes" & $this->to=="Bytes") {
-        $resultado = $this->cantidad * $this->megabytes;
-        echo $this->cantidad . " ". $this->from . " = " . rtrim(number_format($resultado,0,".",",")) . " ". $this->to;
-      }
-      elseif ($this->from=="Megabytes" & $this->to=="Kilobytes") {
-        $resultado = $this->cantidad * $this->kilobytes;
-        echo $this->cantidad . " ". $this->from . " = " . rtrim(number_format($resultado,0,".",",")) . " ". $this->to;
-          }
-      elseif ($this->from=="Megabytes" & $this->to=="Megabytes") {
-        echo $this->cantidad . " ". $this->from . " = " . $this->cantidad . " " . $this->to;
-          }
-      elseif ($this->from=="Megabytes" & $this->to=="Gigabytes") {
-        $resultado = $this->cantidad / $this->kilobytes;
-        echo $this->cantidad . " ". $this->from . " = " . rtrim(number_format($resultado,6,".",",")) . " ". $this->to;
-          }
-      elseif ($this->from=="Megabytes" & $this->to=="Terabytes") {
-        $resultado = $this->cantidad / $this->megabytes;
-        echo $this->cantidad . " ". $this->from . " = " . rtrim(number_format($resultado,10,".",",")) . " ". $this->to;
-          }
-//GIGABYTES A OTRAS UNIDADES          
-      elseif ($this->from=="Gigabytes" & $this->to=="Bytes") {
-        $resultado = $this->cantidad * $this->megabytes;
-        echo $this->cantidad . " ". $this->from . " = " . rtrim(number_format($resultado,0,".",",")) . " ". $this->to;
-        }
-      elseif ($this->from=="Gigabytes" & $this->to=="Kilobytes") {
-        $resultado = $this->cantidad * $this->gigabytes;
-        echo $this->cantidad . " ". $this->from . " = " . rtrim(number_format($resultado,0,".",",")) . " ". $this->to;
-        }
-      elseif ($this->from=="Gigabytes" & $this->to=="Megabytes") {
-        $resultado = $this->cantidad * $this->kilobytes;
-        echo $this->cantidad . " ". $this->from . " = " . rtrim(number_format($resultado,0,".",",")) . " ". $this->to;
-          }
-      elseif ($this->from=="Gigabytes" & $this->to=="Gigabytes") {
-        echo $this->cantidad . " ". $this->from . " = " . $this ->cantidad . " " . $this->to;
-          }
-      elseif ($this->from=="Gigabytes" & $this->to=="Terabytes") {
-        $resultado = $this->cantidad / $this->kilobytes;
-        echo $this->cantidad . " ". $this->from . " = " . rtrim(number_format($resultado,6,".",",")) . " ". $this->to;
-          }
-//TERABYTES A OTRAS UNIDADES          
-      elseif ($this->from=="Terabytes" & $this->to=="Bytes") {
-        $resultado = $this->cantidad * $this->terabytes;
-        echo $this->cantidad . " ". $this->from . " = " . rtrim(number_format($resultado,0,".",",")) . " ". $this->to;
-          }
-      elseif ($this->from=="Terabytes" & $this->to=="Kilobytes") {
-        $resultado = $this->cantidad * $this->gigabytes;
-        echo $this->cantidad . " ". $this->from . " = " . rtrim(number_format($resultado,0,".",",")) . " ". $this->to;
-            }
-      elseif ($this->from=="Terabytes" & $this->to=="Megabytes") {
-        $resultado = $this->cantidad * $this->megabytes;
-        echo $this->cantidad . " ". $this->from . " = " . rtrim(number_format($resultado,0,".",",")) . " ". $this->to;
-            }
-      elseif ($this->from=="Terabytes" & $this->to=="Gigabytes") {
-        $resultado = $this->cantidad * $this->kilobytes;
-        echo $this->cantidad . " ". $this->from . " = " . rtrim(number_format($resultado,0,".",",")) . " ". $this->to;
-            }
-      elseif ($this->from=="Terabytes" & $this->to=="Terabytes") {
-        echo $this->cantidad . " ". $this->from . " = " . $this->cantidad . " " . $this->to;
-            }
-      }
+  public $bytes = 1;
+  public $kilobytes = 1024;
+  public $megabytes = 1048576;
+  public $gigabytes = 1073741824;
+  public $terabytes = 1099511627776;
+  
+  public function Convertir(){
+    if (isset($_POST['enviar'])){
+      $cantidad = $_POST['cantidad'];   
+      $from = $_POST['from_unit'];
+      $to = $_POST['to_unit'];
+//// PEDIR CANTIDAD Y CONVERTIR LA SELECCION "FROM" A BYTE
+     switch ($from) {
+       case 'Bytes':
+        $converBytes = $cantidad * 1;
+         break;
+         case 'Kilobytes':
+          $converBytes = $cantidad * 1024;
+         break;
+         case "Megabytes":
+          $converBytes = $cantidad * 1048576;
+         break;
+         case "Gigabytes":
+          $converBytes = $cantidad * 1073741824;
+         break;
+         case "Terabytes":
+         $converBytes = $cantidad * 1099511627776;
+         break; }
+////CONVERTIR A UNIDAD SELECCIONADA EN TO
+       switch ($to){
+        case 'Bytes':
+          $ConvertirUnidad = $converBytes / 1;
+          echo $cantidad." ".$from." equivale ".$ConvertirUnidad." ".$to;
+         break;
+        case "Kilobytes":
+          $ConvertirUnidad = $converBytes / 1024;
+          echo $cantidad." ".$from." equivale a ".$ConvertirUnidad." ".$to;
+        break;
+        case "Megabytes":
+          $ConvertirUnidad = $converBytes / 1048576;
+          echo $cantidad." ".$from." equivale a ".$ConvertirUnidad." ".$to;
+        break;
+        case "Gigabytes":
+          $ConvertirUnidad = $converBytes / 1073741824;
+          echo $cantidad." ".$from." equivale a ".$ConvertirUnidad." ".$to;
+        break;
+        case "Terabytes":
+          $ConvertirUnidad = $converBytes / 1099511627776;
+          echo $cantidad." ".$from." equivale a ".$ConvertirUnidad." ".$to;
+        break; 
     }
+  }
+  }
 }
 
 class Hija extends Padre{
-    
 }
 
 $prueba = new Hija;
 $prueba->Convertir();
-/*$prueba->KiloBytes();
-$prueba->MegaBytes();
-$prueba->GigaBytes();
-$prueba->TeraBytes();*/
 
 ?>
