9fd9c7fe0023ee507980c6b987a0a0576f85010c
katherine chavarria <katherinechavarria201@gmail.com>
2022-05-12 22:30:08 -0600

    arreglo de archivos

---
+++ src/php/Distancia/capturadedatos.php
@@ -0,0 +1,33 @@
+<?php
+///EN ESTA CLASE SE TOMARAN LOS DATOS INGRESADOS POR EL USUARIO
+
+ 
+  class Distancias {
+    const millimeters= 0.001;
+    const centimeters= 0.01;
+    const meters= 1;
+    const kilometers=1000;
+    const miles= 1609;
+
+    public $cantidad=null;
+    public $from=null;
+    public $to=null;
+    public $converM = null;
+    
+
+    public  function Captura (){
+        if (isset($_POST['enviar'])) {
+            $this->cantidad = $_POST['cantidad'];
+            $this->from = $_POST['from_unit'];
+            $this->to = $_POST['to_unit'];
+            $this->converM;
+            
+            return $this->cantidad;
+            return $this->from;
+            return $this->to;
+            return $this->converM;
+           
+        }       
+    }
+
+}
